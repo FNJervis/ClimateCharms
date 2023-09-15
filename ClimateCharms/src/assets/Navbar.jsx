c52ed7be33d4a9ae615054929794931f4105a887
Erick Hernández <erickhdz17@gmail.com>
2023-09-14 19:25:31 -0500

    Login and Cart

---
+++ ClimateCharms/src/assets/Navbar.jsx
@@ -0,0 +1,38 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {Link} from "react-router-dom"
+import reactLogo from './react.svg';
+import CartIcon from './cart.png'
+
+function NavigationBar() {
+    return (
+        <Navbar expand="lg" data-bs-theme="dark"  className="bg-body-tertiary">
+            <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            src={reactLogo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                    </Navbar.Brand>
+                <Navbar.Brand href="#home">Climate Charms</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        {/*Agregar link a tienda y home*/}
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/Shop">Catálgo</Nav.Link>
+                        <Nav.Link as={Link} to="/">Login</Nav.Link>
+                        <Nav.Link as={Link} to="/Cart">Carrito</Nav.Link>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavigationBar;
---
+++ ClimateCharms/src/products.js
@@ -0,0 +1,61 @@
+import product1 from "./assets/cart.png";
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Pulsera 1",
+        price: 100.0,
+        productImage: product1,
+        temp: 35,
+    },
+    {
+        id: 2,
+        productName: "Pulsera 2",
+        price: 100.0,
+        productImage: product1,
+        temp: 35,
+    },
+    {
+        id: 3,
+        productName: "Pulsera 3",
+        price: 100.0,
+        productImage: product1,
+        temp: 35,
+    },
+    {
+        id: 4,
+        productName: "Pulsera 4",
+        price: 100.0,
+        productImage: product1,
+        temp: 35,
+    },
+    {
+        id: 5,
+        productName: "Pulsera 5",
+        price: 100.0,
+        productImage: product1,
+        temp: 35,
+    },
+    {
+        id: 6,
+        productName: "Pulsera 6",
+        price: 100.0,
+        productImage: product1,
+        temp: 35,
+    },
+    {
+        id: 7,
+        productName: "Pulsera 7",
+        price: 100.0,
+        productImage: product1,
+        temp: 35,
+    },
+    {
+        id: 8,
+        productName: "Pulsera 8",
+        price: 100.0,
+        productImage: product1,
+        temp: 35,
+    },
+];
