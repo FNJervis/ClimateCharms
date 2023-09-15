f6861e8f360b893a4eceb5f9f2ab9aa242739058
AGal04 <113276098+AGal04@users.noreply.github.com>
2023-09-14 22:37:32 -0600

    Ya agregue las imágenes, ¿cómo te salen a ti?

--- ClimateCharms/src/products.js
+++ ClimateCharms/src/products.js
@@ -1,61 +1,67 @@
-import product1 from "./assets/cart.png";
-
+import pulsera_1 from "./assets/pulsera_1.jpg";
+import pulsera_2 from "./assets/pulsera_2.jpg";
+import pulsera_3 from "./assets/pulsera_3.jpg";
+import pulsera_4 from "./assets/pulsera_4.jpg";
+import pulsera_5 from "./assets/pulsera_5.jpg";
+import pulsera_6 from "./assets/pulsera_6.jpg";
+import pulsera_7 from "./assets/pulsera_7.jpg";
+import pulsera_8 from "./assets/pulsera_8.jpg";
 
 export const PRODUCTS = [
     {
         id: 1,
         productName: "Pulsera 1",
         price: 100.0,
-        productImage: product1,
+        productImage: pulsera_1,
         temp: 35,
     },
     {
         id: 2,
         productName: "Pulsera 2",
         price: 100.0,
-        productImage: product1,
+        productImage: pulsera_2,
         temp: 35,
     },
     {
         id: 3,
         productName: "Pulsera 3",
         price: 100.0,
-        productImage: product1,
+        productImage: pulsera_3,
         temp: 35,
     },
     {
         id: 4,
         productName: "Pulsera 4",
         price: 100.0,
-        productImage: product1,
+        productImage: pulsera_4,
         temp: 35,
     },
     {
         id: 5,
         productName: "Pulsera 5",
         price: 100.0,
-        productImage: product1,
+        productImage: pulsera_5,
         temp: 35,
     },
     {
         id: 6,
         productName: "Pulsera 6",
         price: 100.0,
-        productImage: product1,
+        productImage: pulsera_6,
         temp: 35,
     },
     {
         id: 7,
         productName: "Pulsera 7",
         price: 100.0,
-        productImage: product1,
+        productImage: pulsera_7,
         temp: 35,
     },
     {
         id: 8,
         productName: "Pulsera 8",
         price: 100.0,
-        productImage: product1,
+        productImage: pulsera_8,
         temp: 35,
     },
 ];
