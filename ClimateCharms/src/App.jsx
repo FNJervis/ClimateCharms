10cb6417c8c39962478a4ee920b585d93a1b7f36
F. N. Jervis <nicojervis@gmail.com>
2023-09-15 00:16:13 -0600

    merge conflict test

--- ClimateCharms/src/App.jsx
+++ ClimateCharms/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavigationBar from "./assets/Navbar.jsx";
+import {Route,Routes} from "react-router-dom"
+import {BrowserRouter} from "react-router-dom"
+import Login from './Login.jsx';
+import {Shop} from "./pages/shop/shop.jsx";
+import {Cart} from "./pages/cart/cart.jsx";
+import { ShopContextProvider } from "./context/shop-context";
+import Home from "./pages/home/Home.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <ShopContextProvider>
+          <BrowserRouter>
+              <NavigationBar/>
+          <Routes> {/*Agregar ruta de home y tienda*/}
+              <Route path="/" element={<Home/>}/>
+              <Route path="/login" element={<Login/>}/>
+              <Route path="/shop" element={<Shop/>}/>
+              <Route path="/cart" element={<Cart/>}/>
+              <Route path="/home" element={<Home/>}/>
+          </Routes>
+          </BrowserRouter>
+          </ShopContextProvider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
   )
 }
-
-export default App
+export default App;
