cda2eab11594754fcd2366c94280748847a76891
F. N. Jervis <nicojervis@gmail.com>
2023-09-14 23:47:06 -0600

    changes signin component

---
+++ ClimateCharms/src/signin.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import {auth,provider} from "./config";
+import {signInWithPopup} from "firebase/auth";
+import Home from "./Home";
+
+function SignIn(){
+    const [value,setValue] = useState('')
+    const handleClick =()=>{
+        signInWithPopup(auth,provider).then((data)=>{
+            setValue(data.user.email)
+            localStorage.setItem("email",data.user.email)
+        })
+    }
+
+    useEffect(()=>{
+        setValue(localStorage.getItem('email'))
+    })
+
+return (
+    <div>
+        {value?<Home/>:
+        <button onClick={handleClick}>Iniciar Sesión con Google</button>
+        }
+    </div>
+)
+    }
